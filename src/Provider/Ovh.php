27038f2f1e4c75665a4919f5c6b7316af0852160
Ronan Giron <ElGigi@users.noreply.github.com>
2025-05-21 11:12:57 +0200

    Update OVH provider with new unified endpoint

--- src/Provider/Ovh.php
+++ src/Provider/Ovh.php
@@ -12,22 +12,6 @@ readonly class Ovh extends AbstractProvider
 {
     protected function createUri(CompletionInterface $completion): UriInterface
     {
-        $model = strtolower($completion->getModel() ?? $this->model);
-        $model = str_replace(['.', ' '], '-', $model);
-
-        return Uri::createFromString(
-            sprintf(
-                'https://%s.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1/chat/completions',
-                $model
-            )
-        );
-    }
-
-    protected function createBody(CompletionInterface $completion): array
-    {
-        $body = parent::createBody($completion);
-        $body['model'] = null;
-
-        return $body;
+        return Uri::createFromString('https://oai.endpoints.kepler.ai.cloud.ovh.net/v1/chat/completions');
     }
 }
